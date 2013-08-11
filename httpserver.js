68d88b295228ad08d0adf824d971a1c31b5be114
ifollend <ivan.follender@hbo.com>
2013-08-11 18:09:01 -0400

    KikoExperiment2

--- httpserver.js
+++ httpserver.js
@@ -1,40 +1,58 @@
-load ('vertx.js')
-load ('/Users/ifollend/tddTraining/tddjs/source/KikoExperiment/jquery.js')
+var vertx = require('vertx/http');
+var console = require('vertx/console');
 
+console.log("hello server");
+
+
+/**
+  Create an HttpServer to server static web pages so we no longer have to load the from the local file system.
+  This should finally eliminate the CORS issue!!
+*/
 vertx.createHttpServer().requestHandler(function(req) {
-            var webroot = '/Users/ifollend/tddTraining/tddjs/';
-                                        
-                                        
-                                        req.response.putHeader("Access-Control-Allow-Origin", "*");
-                                        $.get("http://catalog.lv3.hbogo.com/apps/mediacatalog/rest/landingService/HBO/landing/DO", function(data) {
-                                              var returnHtml = "";
-                                              var sections = $(data).find("sectionContentResponses");
-                                              $(sections).each( function(index) {
-                                                               var titleText = ($(this).find("title"))[0].textContent;
-                                                               
-                                                               var images = $(this).find("resourceUrl");
-                                                               $(images).each( function(index) {
-                                                                              var imageUrl = $(images)[index].textContent;
-                                                                              if ( ( imageUrl.indexOf("default") <= 0 ) && ( imageUrl.indexOf("754x424") >= 0 ) ) {
-                                                                              returnHmtl = returnHtml + "<img src=" + imageUrl + " title= \"" + titleText + "\"\/>";
-                                                                              };
-                                                                              });
-                                                               
-                                                               
-                                                               });
-                                              
-                                              });
-                                        req.response.write(returnHtml);
 
-                                        
-        }).listen(8080, "localhost");
+  console.log("responding to static content request: " + req.path());
 
-function comments() {
-    var file = webroot + req.path;
-    if (file.indexOf('.js') > 0) {
-        req.response.putHeader("Content-type", "application/json").putHeader("Access-Control-Allow-Origin", "*");
+  var webRoot = '/Users/ifollend/tddTraining/tddjs/source/KikoExperiment3/';
+
+  if (req.path().indexOf('/data') < 0) {
+    // Send the file back based on webRoot and the path requested
+    req.response.sendFile(webRoot + req.path());
+  } else {
+    console.log("responding to data request: " + req.path());
+
+    // Create the HttpClient to call out to catalog
+    var client = vertx.createHttpClient().port(80).host("catalog.lv3.hbogo.com");
+
+    // Call the landing service and return it in the response
+    client.getNow('/apps/mediacatalog/rest/landingService/HBO/landing/DO', function(resp) {
+      resp.bodyHandler(function(body) {
+        console.log('The total body received was ' + body.length() + ' bytes');
+
+        req.response.end(body);
+      });
+
+    });
+  }
+
+}).listen(8080, "localhost", function(err) { // Add a callback to confirm the server is listenting
+    if (!err) {
+        console.log("Listen succeeded! on 8080");
     }
-    file = req.path === '/' ? 'index.html' : req.path;
-    req.response.sendFile('/Users/ifollend/tddTraining/tddjs/source/KikoExperiment/sampleclient.html');
+  }
+);
 
-}
+
+/**
+  Create an HttpServer that is a HttpClient to catalog to actually get the data.
+  This server sets the Access-Control-Allow-Origin header so the browser doesn't have to worry about CORS
+*/
+vertx.createHttpServer().requestHandler(function(req, landingPage) {
+
+
+}).listen(8085, "localhost", function(err) { // Add a callback to confirm the server is listenting
+    if (!err) {
+        console.log("Listen succeeded! on 8085");
+    }
+  }
+);
+
